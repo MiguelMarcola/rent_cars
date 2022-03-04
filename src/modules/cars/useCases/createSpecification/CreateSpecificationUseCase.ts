4bc714f6f02e694120b978a5862f6502c6da4488
MiguelMarcola <miguelmarcola@gmail.com>
2022-03-04 11:20:02 -0300

    refatoring code

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -22,7 +22,7 @@ class CreateSpecificationUseCase {
             throw new Error("Specification alredy exists!");
         }
 
-        this.specificationRepository.create({ name, description });
+        await this.specificationRepository.create({ name, description });
     }
 }
 
